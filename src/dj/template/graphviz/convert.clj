049411f7d5d70d7ca9088fcce2ece7176009c86c
Brent Millare <bmillare@amazon.com>
2020-04-26 01:09:56 +0000

    fixed graphviz helper name

--- src/dj/template/graphviz/convert.clj
+++ src/dj/template/graphviz/convert.clj
@@ -1,6 +1,6 @@
 (ns dj.template.graphviz.convert)
 
-(defn dependencies->rev-edges
+(defn direct-dependencies-map->edges
   "converts direct dependency graph (key->vec of keys) into a graphviz
   friendly set of edges (each edge is a vector representing a keyword
   pair) representing keyword1 \"needed by\" keyword2
